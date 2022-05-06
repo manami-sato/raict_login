0118bee2dbb51294c3622d723934937a9b412124
sato manami <manamisato2001@gmail.com>
2022-05-06 12:23:43 +0900

    updated: github pagesに移行

--- src/mixins/Mixin.vue
+++ src/mixins/Mixin.vue
@@ -2,7 +2,7 @@
 export default {
   data() {
     return {
-      path: "https://click.ecc.ac.jp/ecc/msatou/raict_login/",
+      path: "https://manami-sato.github.io/raict_login/",
       appImgPath: "https://manami-sato.github.io/raict_login/src/assets/img/",
       productsData: "https://click.ecc.ac.jp/ecc/msatou/products/raict.php",
     };
